b5f02521cfd7fed451d29c81b67dd9dae0452441
lokmanACH <lokmanelhakimachouche0@gmail.com>
2026-05-18 19:15:36 +0100

    third shot

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
@@ -6,14 +5,8 @@ import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath, URL } from "node:url";
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> ff64db5053116e130694ae8ddd0ff07d4b45be9f
 
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     tanstackStart(),
     nitro(),
@@ -29,7 +22,3 @@ export default defineConfig({
     dedupe: ["react", "react-dom", "@tanstack/react-router"],
   },
 });
-=======
-  plugins: [react()],
-})
->>>>>>> ff64db5053116e130694ae8ddd0ff07d4b45be9f
